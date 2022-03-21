f3123acec5632c25729b20471bbd2925d991e350
bansalji02 <hb13april2002@gmail.com>
2022-03-21 22:38:48 +0530

    made the manageproductitem screen

--- lib/screens/manage_products_screen.dart
+++ lib/screens/manage_products_screen.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/product.dart';
+import 'package:flutter_complete_guide/providers/products_provider.dart';
 import 'package:flutter_complete_guide/widget/app_drawer.dart';
+import 'package:flutter_complete_guide/widget/manage_product_item.dart';
+import 'package:provider/provider.dart';
 
 class ManageProductsScreen extends StatelessWidget {
   static const routename = '/manage-products-screen';
 
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+
     return Scaffold(
-      drawer: AppDrawer(),
-      appBar: AppBar(
-        title: Text('Manage Products!'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
-      ),
-      body: Center(
-        child: Text('MANage screen'),
-      ),
-    );
+        drawer: AppDrawer(),
+        appBar: AppBar(
+          title: Text('Manage Products!'),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(8),
+            child: ListView.builder(
+              itemCount: productsData.items.length,
+              itemBuilder: (ctx, index) => ManageProductItem(title: productsData.items[index].title , imageUrl: productsData.items[index].imageUrl),
+          ),
+        ));
   }
 }
---
+++ lib/widget/manage_product_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+class ManageProductItem extends StatelessWidget {
+
+  final String title;
+  final String imageUrl;
+
+  ManageProductItem({ @required this.title,@required this.imageUrl});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing:Container(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.edit),),
+            IconButton(onPressed: (){}, icon: Icon(Icons.delete),),
+          ],
+        ),
+      ),
+    );
+  }
+}
